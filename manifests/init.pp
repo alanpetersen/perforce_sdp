0ee2205ee09fa29c1522f4463a364d15a92b8092
alanpetersen <alanpetersen@mac.com>
2015-09-25 13:36:26 -0700

    cleaning things up

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,19 @@
 #
 # Sample Usage:
 #
-class perforce_sdp {
-  
+class perforce_sdp (
+  $osuser           = $perforce_sdp::params::osuser,
+  $osgroup          = $perforce_sdp::params::osgroup,
+  $adminuser        = $perforce_sdp::params::adminuser,
+  $adminpass        = $perforce_sdp::params::adminpass,
+  $p4_dir           = $perforce_sdp::params::p4_dir,
+  $depotdata_dir    = $perforce_sdp::params::depotdata_dir,
+  $metadata_dir     = $perforce_sdp::params::metadata_dir,
+  $logs_dir         = $perforce_sdp::params::logs_dir,
+  $sslprefix        = $perforce_sdp::params::sslprefix,
+  $p4_version       = $perforce_sdp::params::p4_version,
+  $p4d_version      = $perforce_sdp::params::p4d_version,
+  $p4broker_version = $perforce_sdp::params::p4broker_version,
+) inherits perforce_sdp::params {
+  include perforce_sdp::client
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,4 @@
 class perforce_sdp::params {
-  
   $osuser = 'perforce'
   $osgroup = 'perforce'
   $adminuser = 'p4admin'
@@ -12,23 +11,26 @@ class perforce_sdp::params {
   $p4_version = '2015.1'
   $p4d_version = '2015.1'
   $p4broker_version = '2015.1'
-    
+
   $p4_version_short = regsubst($p4_version, '^20', '', 'G')
   $p4d_version_short = regsubst($p4d_version, '^20', '', 'G')
   $p4broker_version_short = regsubst($p4broker_version, '^20', '', 'G')
-  
-  if $::kernel == 'Linux' {
-    if $::kernelmajversion == '2.6' {
-      if $::os[architecture] == "x86_64" {
-        $dist_dir = 'bin.linux26x86_64'
-      } else {
-        $dist_dir = 'bin.linux26x86'
+
+  case $::kernel {
+    'Linux': {
+      if $::kernelmajversion == '2.6' {
+        if $::os[architecture] == "x86_64" {
+          $dist_dir = 'bin.linux26x86_64'
+        } else {
+          $dist_dir = 'bin.linux26x86'
+        }
       }
     }
-  } elsif $::kernel == 'Windows' {
-    
-  } else {
-    fail("Kernel OS ${::kernel} is not suppported")
+    'Windows': {
+
+    }
+    default: {
+      fail("Kernel OS ${::kernel} is not suppported")
+    }
   }
-  
 }
